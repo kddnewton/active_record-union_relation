e242c9129f4163d7a112898ae9bf65c6eef1c68a
Kevin Deisz <kevin.deisz@gmail.com>
2020-10-08 12:38:03 -0400

    Add code

--- test/active_record/union_test.rb
+++ test/active_record/union_test.rb
@@ -6,4 +6,23 @@ class ActiveRecord::UnionTest < Minitest::Test
   def test_version
     refute_nil ::ActiveRecord::Union::VERSION
   end
+
+  def test_union
+    term = 'foo'
+    relation =
+      Post.union(:id, :post_id, :matched) do |union|
+        posts = Post.where(published: true).where('title LIKE ?', "%#{term}%")
+        comments = Comment.where('body LIKE ?', "%#{term}%")
+        tags = Tag.where('name LIKE ?', "%#{term}%")
+    
+        union.add posts,    :id, nil,      :title
+        union.add comments, :id, :post_id, :body
+        union.add tags,     :id, nil,      :name
+      end
+
+    unioned = relation.order(matched: :asc).group_by(&:class)
+    assert_equal 3, unioned.length
+
+    assert_kind_of Post, unioned[Comment][0].post
+  end
 end
