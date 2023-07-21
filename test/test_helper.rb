e2cc13f388ba3da9092e18eb4e4191949fb401f2
Kevin Newton <kddnewton@gmail.com>
2023-07-21 10:32:06 -0400

    Commit fixtures in transaction

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,6 +12,10 @@ ActiveRecord::Base.establish_connection
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 ActiveRecord::Schema.define do
+  create_table :tags, force: true do |t|
+    t.string :name
+  end
+
   create_table :posts, force: true do |t|
     t.boolean :published
     t.string :title
@@ -21,32 +25,34 @@ ActiveRecord::Schema.define do
     t.references :post
     t.text :body
   end
+end
 
-  create_table :tags, force: true do |t|
-    t.string :name
-  end
+class Tag < ActiveRecord::Base
+end
+
+class Post < ActiveRecord::Base
+  has_many :comments
+  accepts_nested_attributes_for :comments
 end
 
 class Comment < ActiveRecord::Base
   belongs_to :post
 end
 
-class Post < ActiveRecord::Base
-  has_many :comments
-  accepts_nested_attributes_for :comments
-
-  create!(published: false, title: "foo not published")
-  create!(
-    published: true,
-    title: "foo published",
-    comments_attributes: [
-      { body: "This is a comment" },
-      { body: "This is another comment" },
-      { body: "This is a comment with foo in it" }
+ActiveRecord::Base.transaction do
+  Tag.create!([{ name: "some" }, { name: "tags" }, { name: "foo" }])
+  Post.create!(
+    [
+      { published: false, title: "foo not published" },
+      {
+        published: true,
+        title: "foo published",
+        comments_attributes: [
+          { body: "This is a comment" },
+          { body: "This is another comment" },
+          { body: "This is a comment with foo in it" }
+        ]
+      }
     ]
   )
-end
-
-class Tag < ActiveRecord::Base
-  create!([{ name: "some" }, { name: "tags" }, { name: "foo" }])
 end
